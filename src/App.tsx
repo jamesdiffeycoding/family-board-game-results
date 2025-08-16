991457320d2b93a97c8ac00af6d5be76f61c245c
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-08-16 13:51:11 +0100

    working supabase fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import "./App.css";
+import { supabase } from "./supabase-client";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="text-red-500">This is the homepage</div>;
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      let { data: people, error } = await supabase
+        .from("tm_people") // 👈 replace with your table name
+        .select("*"); // select all columns
+
+      if (error) {
+        console.error(error);
+      } else {
+        setPeople(people);
+      }
+      setLoading(false);
+    };
+
+    fetchPeople();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Data from Supabase</h1>
+      <ul>
+        {people.map((item) => (
+          <li key={item.id}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
